8f314d902f43cca84bb94608107a9ee2982548cb
ph4 <22990490+ph4@users.noreply.github.com>
2021-05-13 11:38:46 +0300

    Check for db connection at initialization

--- BaseModelCtx.cs
+++ BaseModelCtx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace kurs
 {
@@ -18,6 +19,15 @@ namespace kurs
         public static Dns2Entities GetContext()
         {
             if (_context == null) _context = new Dns2Entities();
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error initializing, no db connection?");
+                return null;
+            }
             return _context;
         }
 
